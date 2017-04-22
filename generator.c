80be5b03ffe86c04b1ba6fe246b3bedf6f70195e
DanielRosival <daniel.rosival@gmail.com>
2017-04-22 23:21:12 +0200

    update

--- generator.c
+++ generator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -22,8 +23,6 @@ int main(int argc, char const *argv[])
         return 1;
     }
     
-    n = argv[2];
-    
     f = open ( argv[1], O_WRONLY | O_CREAT, 0660 );
     if ( f < 0 ){
         return 1;
@@ -31,19 +30,19 @@ int main(int argc, char const *argv[])
     
     srand ( time( NULL ) );
     
-    for (i = 0; i <= n; ++i){
+    for (i = 0; i <= (*argv[2]); ++i){
         l = rand() % 10 + ( dolna_hranica + 1 );
-        write ( f, n, sizeof(char) * 1 );
+        write ( f, argv[2], sizeof(char) * 1 );
         for (j = 0; i <= l; ++j){
             *number = (float) rand() / (float) RAND_MAX;
             write ( f, number, sizeof(float) * 1 );
         }
     }
     
-    close ( f );
-    
-    
-    
+    if ( close (f) != 0) {
+    	printf("FAILED\n");
+    	printf("%s\n", strerror(errno));
+    }
     
     return 0;
 }
