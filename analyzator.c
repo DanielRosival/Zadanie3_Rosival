7631fa908a07daafbc43b16bc5c127814af78119
DanielRosival <daniel.rosival@gmail.com>
2017-04-23 07:39:26 +0200

    analyzator

---
+++ analyzator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
