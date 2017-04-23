9d7a030c28a881314279d59e869d2647ada742a0
DanielRosival <daniel.rosival@gmail.com>
2017-04-23 07:27:28 +0200

    update

--- generator.c
+++ generator.c
@@ -16,26 +16,31 @@ int main(int argc, char const *argv[])
     char n;
     unsigned char l;
     int f,i,j;
-    float *number = NULL;
+    float number;
     
     if ( argc > 3 ){
         printf("Too much arguments.\n");
         return 1;
     }
+
+    if ( argc < 3 ){
+    	printf("Not enough arguments.\n");
+    	return 2;
+    }
     
     f = open ( argv[1], O_WRONLY | O_CREAT, 0660 );
     if ( f < 0 ){
-        return 1;
+        return 3;
     }
     
     srand ( time( NULL ) );
     
-    for (i = 0; i <= (*argv[2]); ++i){
+    for (i = 0; i < (*argv[2]); ++i){
         l = rand() % 10 + ( dolna_hranica + 1 );
-        write ( f, argv[2], sizeof(char) * 1 );
-        for (j = 0; i <= l; ++j){
-            *number = (float) rand() / (float) RAND_MAX;
-            write ( f, number, sizeof(float) * 1 );
+        write ( f, &l, sizeof(char) * 1 );
+        for (j = 0; j < l; ++j){
+            number = (float) rand() / (float) RAND_MAX;
+            write ( f, &number, sizeof(float) * 1 );
         }
     }
     
@@ -43,6 +48,6 @@ int main(int argc, char const *argv[])
     	printf("FAILED\n");
     	printf("%s\n", strerror(errno));
     }
-    
+
     return 0;
 }
